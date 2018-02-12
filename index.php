68f5671fa0418a303985ad6242bf93241afb03b3
Petr <hipon3hidra@gmail.com>
2018-02-13 02:50:24 +0700

    xxxyyzzz

--- index.php
+++ index.php
@@ -6,34 +6,6 @@ require_once("vendor/autoload.php");
 $token = "543178842:AAFlEue5eOAMEq3K4fmBHGGLD3wglfX7Gvo";
 $bot = new \TelegramBot\Api\Client($token);
 
-// если бот еще не зарегистрирован - регистрируем
-if(!file_exists("registered.trigger")){ 
-	/**
-	 * файл registered.trigger будет создаваться после регистрации бота. 
-	 * если этого файла нет значит бот не зарегистрирован 
-	 */
-	 
-	// URl текущей страницы
-	$page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-	$result = $bot->setWebhook($page_url);
-	if($result){
-		file_put_contents("registered.trigger",time()); // создаем файл дабы прекратить повторные регистрации
-	}
-}
 
 
-// обязательное. Запуск бота
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
 
-// помощ
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - помощ';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-// запускаем обработку
-$bot->run();
