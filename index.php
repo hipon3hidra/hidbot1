de7cbf70b22c7d441c148028f1bcfc93f95fe89f
Petr <hipon3hidra@gmail.com>
2018-02-13 02:53:45 +0700

    xxxyyzzz

--- index.php
+++ index.php
@@ -7,5 +7,21 @@ $token = "543178842:AAFlEue5eOAMEq3K4fmBHGGLD3wglfX7Gvo";
 $bot = new \TelegramBot\Api\Client($token);
 
 
+// если бот еще не зарегистрирован - регистрируем
+if(!file_exists("registe
+red.trigger")){ 
+	/**
+	 * файл registered.trigger будет создаваться после регистрации бота. 
+	 * если этого файла нет значит бот не зарегистрирован 
+	 */
+	 
+	// URl текущей страницы
+	$page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	$result = $bot->setWebhook($page_url);
+	if($result){
+		file_put_contents("registered.trigger",time()); // создаем файл дабы прекратить повторные регистрации
+	}
+}
+
 
 
