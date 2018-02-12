4b29ebe7888d6c229795392c8b52d032b0b7c412
Petr <hipon3hidra@gmail.com>
2018-02-13 04:13:40 +0700

    xxxyyzzz

--- index.php
+++ index.php
@@ -44,8 +44,9 @@ $bot->command('ping', function ($message) use ($bot) {
 // обязательное. Запуск бота
 $bot->command('start', function ($message) use ($bot) {
 
+    $answer = 'Добро пожаловать! Выберите город в меню';
 $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => "Петропавловск!"], ["text" => "Москва!"]]], true, true);
-    $bot->sendMessage($message->getChat()->getId(), $answer, 'Добро пожаловать! Выберите город в меню', $keyboards);
+    $bot->sendMessage($message->getChat()->getId(), $answer, $keyboards);
 });
 
 // помощ
