a72b8286af94393285db4f15dfc885aac2262488
Petr <hipon3hidra@gmail.com>
2018-02-13 03:42:28 +0700

    xxxyyzzz

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
+
 <?php
+/**
+ * revcom_bot
+ *
+ * @author - Александр Штокман
+ */
 header('Content-Type: text/html; charset=utf-8');
 // подрубаем API
 require_once("vendor/autoload.php");
 
+// дебаг
+if(true){
+	error_reporting(E_ALL & ~(E_NOTICE | E_USER_NOTICE | E_DEPRECATED));
+	ini_set('display_errors', 1);
+}
+
 // создаем переменную бота
 $token = "543178842:AAFlEue5eOAMEq3K4fmBHGGLD3wglfX7Gvo";
-$bot = new \TelegramBot\Api\Client($token);
+$bot = new \TelegramBot\Api\Client($token,null);
 
+if($_GET["bname"] == "revcombot"){
+	$bot->sendMessage("@burgercaputt", "Тест");
+}
 
-// если бот еще не зарегистрирован - регистрируем
-if(!file_exists("registe
-red.trigger")){ 
+// если бот еще не зарегистрирован - регистируем
+if(!file_exists("registered.trigger")){ 
 	/**
 	 * файл registered.trigger будет создаваться после регистрации бота. 
 	 * если этого файла нет значит бот не зарегистрирован 
@@ -21,92 +35,41 @@ red.trigger")){
 	$result = $bot->setWebhook($page_url);
 	if($result){
 		file_put_contents("registered.trigger",time()); // создаем файл дабы прекратить повторные регистрации
-	}
+	} else die("ошибка регистрации");
 }
+
+// Команды бота
+// пинг. Тестовая
+$bot->command('ping', function ($message) use ($bot) {
+	$bot->sendMessage($message->getChat()->getId(), 'pong!');
+});
 
 // обязательное. Запуск бота
 $bot->command('start', function ($message) use ($bot) {
     $answer = 'Добро пожаловать!';
     $bot->sendMessage($message->getChat()->getId(), $answer);
-
-
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			// Выбор города
-			// Reply-Кнопки
-			$bot->command("buttons", function ($message) use ($bot) {
-			$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => 			"Красноярск"], ["text" => "Новосибирск"]]], true, true);
-
-			$bot->sendMessage($message->getChat()->getId(), "Выберите город", false, 					null,null, $keyboard);
-				});
-
-
-
-
-			// Отлов любых сообщений + обработка reply-кнопок
-			$bot->on(function($Update) use ($bot){
-	
-				$message = $Update->getMessage();
-			$mtext = $message->getText();
-			$cid = $message->getChat()->getId();
-	
-			if(mb_stripos($mtext,"Красноярск") !== false){
-			$bot->sendMessage($message->getChat()->getId(), "Вы выбрали город 				Красноярск, можете выбрать интересующую вас категорию");
-			$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => 			"Стимуляторы"], ["text" => "Эйфоретики"]]], true, true);
-			
-
-
-
-
-
-
-
-				}
-				if(mb_stripos($mtext,"Новосибирск") !== false){
-					$bot->sendMessage($message->getChat()->getId(), "Вы 				выбрали город Новосибирск, можете выбрать интересующую вас категорию");
-			$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => 			"Стимуляторы"], ["text" => "Эйфоретики"]]], true, true);
-				}
-			}, function($message) use ($name){
-				return true; // когда тут true - команда проходит
-			});
-
-
-
-
-
-
-
 });
 
-// помощь
+// помощ
 $bot->command('help', function ($message) use ($bot) {
     $answer = 'Команды:
-/help - помощь';
+/help - помощ';
     $bot->sendMessage($message->getChat()->getId(), $answer);
 });
 
+// передаем картинку
+$bot->command('getpic', function ($message) use ($bot) {
+	$pic = "http://aftamat4ik.ru/wp-content/uploads/2017/03/photo_2016-12-13_23-21-07.jpg";
 
+    $bot->sendPhoto($message->getChat()->getId(), $pic);
+});
 
+// передаем документ
+$bot->command('getdoc', function ($message) use ($bot) {
+	$document = new \CURLFile('shtirner.txt');
 
-
-
-
-
+    $bot->sendDocument($message->getChat()->getId(), $document);
+});
 
 // Кнопки у сообщений
 $bot->command("ibutton", function ($message) use ($bot) {
@@ -144,96 +107,89 @@ $bot->on(function($update) use ($bot, $callback_loc, $find_command){
 	return true;
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Выбор города
-// Reply-Кнопки
-$bot->command("buttons", function ($message) use ($bot) {
-	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => "Красноярск"], ["text" => "Новосибирск"]]], true, true);
-
-	$bot->sendMessage($message->getChat()->getId(), "Выберите город", false, null,null, $keyboard);
+// обработка инлайнов
+$bot->inlineQuery(function ($inlineQuery) use ($bot) {
+	mb_internal_encoding("UTF-8");
+	$qid = $inlineQuery->getId();
+	$text = $inlineQuery->getQuery();
+	
+	// Это - базовое содержимое сообщения, оно выводится, когда тыкаем на выбранный нами инлайн
+	$str = "Что другие?
+Свора голодных нищих.
+Им все равно...
+В этом мире немытом
+Душу человеческую
+Ухорашивают рублем,
+И если преступно здесь быть бандитом,
+То не более преступно,
+Чем быть королем...
+Я слышал, как этот прохвост
+Говорил тебе о Гамлете.
+Что он в нем смыслит?
+<b>Гамлет</b> восстал против лжи,
+В которой варился королевский двор.
+Но если б теперь он жил,
+То был бы бандит и вор.";
+	$base = new \TelegramBot\Api\Types\Inline\InputMessageContent\Text($str,"Html");
+	
+	// Это список инлайнов
+	// инлайн для стихотворения
+	$msg = new \TelegramBot\Api\Types\Inline\QueryResult\Article("1","С. Есенин","Отрывок из поэмы `Страна негодяев`");
+	$msg->setInputMessageContent($base); // указываем, что в ответ к этому сообщению надо показать стихотворение
+	
+	// инлайн для картинки
+	$full = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961.jpg"; // собственно урл на картинку 
+	$thumb = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961-150x150.jpg"; // и миниятюра
+	
+	$photo = new \TelegramBot\Api\Types\Inline\QueryResult\Photo("2",$full,$thumb);
+	
+	// инлайн для музыки
+	$url = "http://aftamat4ik.ru/wp-content/uploads/2017/05/mongol-shuudan_-_kozyr-nash-mandat.mp3";
+	$mp3 = new \TelegramBot\Api\Types\Inline\QueryResult\Audio("3",$url,"Монгол Шуудан - Козырь наш Мандат!");
+	
+	// инлайн для видео
+	$vurl = "http://aftamat4ik.ru/wp-content/uploads/2017/05/bb.mp4";
+	$thumb = "http://aftamat4ik.ru/wp-content/uploads/2017/05/joker_5-150x150.jpg";
+	$video = new \TelegramBot\Api\Types\Inline\QueryResult\Video("4",$vurl,$thumb, "video/mp4","коммунальные службы","тут тоже может быть описание");
+	
+	// отправка
+	try{
+		$result = $bot->answerInlineQuery( $qid, [$msg,$photo,$mp3,$video],100,false);
+	}catch(Exception $e){
+		file_put_contents("rdata",print_r($e,true));
+	}
 });
 
+// Reply-Кнопки
+$bot->command("buttons", function ($message) use ($bot) {
+	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => "Власть советам!"], ["text" => "Сиськи!"]]], true, true);
 
+	$bot->sendMessage($message->getChat()->getId(), "тест", false, null,null, $keyboard);
+});
 
-
-// Отлов любых сообщений + обработка reply-кнопок
+// Отлов любых сообщений + обрабтка reply-кнопок
 $bot->on(function($Update) use ($bot){
 	
 	$message = $Update->getMessage();
 	$mtext = $message->getText();
 	$cid = $message->getChat()->getId();
 	
-	if(mb_stripos($mtext,"Красноярск") !== false){
-		$bot->sendMessage($message->getChat()->getId(), "Вы выбрали город Красноярск, можете выбрать интересующую вас категорию");
-			$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => 			"Стимуляторы"], ["text" => "Эйфоретики"]]], true, true);
-			
+	if(mb_stripos($mtext,"Сиськи") !== false){
+		$pic = "http://aftamat4ik.ru/wp-content/uploads/2017/05/14277366494961.jpg";
 
-
-
-
-
-
-
+		$bot->sendPhoto($message->getChat()->getId(), $pic);
 	}
-	if(mb_stripos($mtext,"Новосибирск") !== false){
-		$bot->sendMessage($message->getChat()->getId(), "Вы выбрали город Новосибирск, можете выбрать интересующую вас категорию");
-$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[["text" => 			"Стимуляторы"], ["text" => "Эйфоретики"]]], true, true);
+	if(mb_stripos($mtext,"власть советам") !== false){
+		$bot->sendMessage($message->getChat()->getId(), "Смерть богатым!");
 	}
 }, function($message) use ($name){
 	return true; // когда тут true - команда проходит
 });
 
-
-
-
-
-
-
-
-
 // запускаем обработку
 $bot->run();
 
-echo "bot";
-
-
-
-
-
-
-
+echo "бот";
 
 
 
