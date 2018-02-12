f2bcb889c2110df0501c4e0626cf5817a51c54fa
Petr <hipon3hidra@gmail.com>
2018-02-13 03:21:38 +0700

    xxxyyzzz

--- index.php
+++ index.php
@@ -45,6 +45,63 @@ $bot->command('help', function ($message) use ($bot) {
 
 
 
+// Кнопки у сообщений
+$bot->command("ibutton", function ($message) use ($bot) {
+	$keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+		[
+			[
+				['callback_data' => 'data_test', 'text' => 'Answer'],
+				['callback_data' => 'data_test2', 'text' => 'ОтветЪ']
+			]
+		]
+	);
+
+	$bot->sendMessage($message->getChat()->getId(), "тест", false, null,null,$keyboard);
+});
+
+// Обработка кнопок у сообщений
+$bot->on(function($update) use ($bot, $callback_loc, $find_command){
+	$callback = $update->getCallbackQuery();
+	$message = $callback->getMessage();
+	$chatId = $message->getChat()->getId();
+	$data = $callback->getData();
+	
+	if($data == "data_test"){
+		$bot->answerCallbackQuery( $callback->getId(), "This is Ansver!",true);
+	}
+	if($data == "data_test2"){
+		$bot->sendMessage($chatId, "Это ответ!");
+		$bot->answerCallbackQuery($callback->getId()); // можно отослать пустое, чтобы просто убрать "часики" на кнопке
+	}
+
+}, function($update){
+	$callback = $update->getCallbackQuery();
+	if (is_null($callback) || !strlen($callback->getData()))
+		return false;
+	return true;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
